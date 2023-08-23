6260fda23fe102e7a9fea8d20376aadc2aa18c95
BrianFernandoJara <brianfernandojara@gmail.com>
2023-08-23 09:26:17 -0400

    event listening lab complete

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const theButton = document.getElementById("button");
+
+    function alertDropDown(){
+        alert("This was an alert WOAH")
+    }
+    theButton.addEventListener("click",alertDropDown)
 }
+
+addingEventListener();
